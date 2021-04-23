27f1d6044066a0ec33ec8af69300e8f33d6a13ed
rood187 <rood.pavel@gmail.com>
2021-04-23 12:14:43 +0300

    First commit

---
+++ src/main/java/com/example/sweater_two/SweaterTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweater_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweaterTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sweater_two/SweaterTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweater_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
